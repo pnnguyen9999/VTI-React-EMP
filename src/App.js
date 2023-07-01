d75abdf08948e9fc3bef543bbe26c9f8e8b7a021
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2023-07-01 20:12:59 +0700

    update react router v6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import AccountPage from "./Page/AccountPage";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { routes } from "./Router/routes";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Home from "./Page/Home";
+import { Button } from "reactstrap";
 function App() {
   return (
     <Provider store={store}>
@@ -16,10 +18,35 @@ function App() {
         <li>
           <Link to="/users/">Users</Link>
         </li>
+        <li>
+          <Link to="/form">Form</Link>
+        </li>
       </ul>
-      {routes}
+      <AppLayout>
+        {routes}
+      </AppLayout>
     </Provider>
   );
+}
+
+const AppLayout = ({ children }) => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    const isUserValid = true;
+    if (isUserValid) {
+      navigate('/users/1?status=active&extraInfo=invidual');
+    } else {
+      navigate('/');
+    }
+  };
+  return <>
+    <div>header
+      <Button onClick={handleRedirect}>Navigate to user 1</Button>
+    </div>
+    {children}
+    <div>footer</div>
+  </>;
 }
 
 
--- src/Router/routes.js
+++ src/Router/routes.js
@@ -1,6 +1,8 @@
-import { Link, Route, Routes, useParams } from "react-router-dom";
+import { Link, Route, Routes, useParams, useSearchParams, unstable_usePrompt } from "react-router-dom";
 import AccountPage from "../Page/AccountPage";
 import Home from "../Page/Home";
+import { Button } from "reactstrap";
+import { useState } from "react";
 
 
 const Users = () => {
@@ -15,23 +17,61 @@ const Users = () => {
 };
 
 const UserList = () => {
+
     return <div>
         <div>
-            <Link to="/users/1">User 1</Link>
-            <Link to="/users/2">User 2</Link>
+            <Link to="/users/1?status=active&extraInfo=invidual">User 1</Link>
+            <Link to="/users/an.nguyen?status=disable&extraInfo=admin">User 2</Link>
         </div>
     </div>;
 };
 
 const UserDetail = () => {
     const params = useParams();
-    console.log(params);
     const { id } = params;
-    return <div>User detail: {id}</div>;
+
+    const [searchParams] = useSearchParams();
+    const status = searchParams.get('status');
+    const extraInfo = searchParams.get('extraInfo');
+
+    return <>
+        <div>User detail: {id}</div>
+        <div>User status: {status}</div>
+        <div>User extra info: {extraInfo}</div>
+    </>;
 };
+
+const FormTest = () => {
+    const [user, setUser] = useState("");
+    const [email, setEmail] = useState("");
+
+    const [isDraft, setIsDraft] = useState(false);
+
+    unstable_usePrompt("Hello from usePrompt -- Are you sure you want to leave?", isDraft);
+
+    const handleSave = () => {
+        setIsDraft(false);
+        console.log({ user, email });
+    };
+    return <div>
+        <h2>Form</h2>
+        <form>
+            <label>
+                Tên:
+                <input type="text" name="name" value={user} onChange={(e) => { setUser(e.target.value); setIsDraft(true); }} />
+            </label>
+            <label>
+                Email:
+                <input type="text" name="email" value={email} onChange={(e) => { setEmail(e.target.value); setIsDraft(true); }} />
+            </label>
+        </form>
+        <Button onClick={handleSave}>Lưu</Button>
+    </div>;
+}
 
 export const routes = <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/account-management" element={<AccountPage />} />
     <Route path="/users/*" element={<Users />} />
+    <Route path="/form" element={<FormTest />} />
 </Routes>;
